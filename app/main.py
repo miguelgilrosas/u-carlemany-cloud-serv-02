d24b2b163f5c63440abe4cb3bd20104ff3453c39
miguelgilrosas <miguelgilrosas@gmail.com>
2025-03-13 22:57:39 +0000

    files logic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.authentication.router import router as authentication_router
-from app.files_app.router import router as files_router
+from app.files.router import router as files_router
 
 app = FastAPI()
 app.include_router(authentication_router, prefix='/auth')
