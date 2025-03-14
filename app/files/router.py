2fa3173379812270f5b50af4216a1112ecde6456
miguelgilrosas <miguelgilrosas@gmail.com>
2025-03-14 00:26:42 +0000

    activity 2

--- app/files/router.py
+++ app/files/router.py
@@ -3,6 +3,7 @@ from pypdf import PdfMerger
 import uuid
 from pydantic import BaseModel
 import aiohttp
+import json
 
 router = APIRouter()
 
@@ -14,55 +15,7 @@ files = {}
 class FileModel(BaseModel):
     filename: str
     path: str
-
-
-@router.get("/")
-async def get_files() -> dict[str, str]:
-    return {"status": "ok"}
-
-
-@router.post("/")
-async def post_file(
-    auth: str = Header(),
-    input_file: UploadFile = File()
-) -> dict[str, FileModel]:
-    auth_response = await introspect(auth=auth)
-    if auth_response is None:
-        raise HTTPException(status_code=403, detail='Forbidden')
-
-    prefix = 'files/'
-    file_id = str(uuid.uuid4())
-    while file_id in files:
-        file_id = str(uuid.uuid4())
-    path = prefix + file_id + '.pdf'
-
-    with open(path, "wb") as buffer:
-        while chunk := await input_file.read(8192):
-            buffer.write(chunk)
-
-    new_file = FileModel(
-        filename=input_file.filename,
-        path=path,
-    )
-    files[file_id] = new_file
-
-    return {file_id: new_file}
-
-
-@router.get("/file/{id}")
-async def get_file_by_id() -> dict[str, str]:
-    return {"status": "ok"}
-
-
-@router.post("/file/{id}")
-async def post_file_by_id() -> dict[str, str]:
-    return {"status": "ok"}
-
-
-@router.delete("/file/{id}")
-async def delete_file_by_id() -> dict[str, str]:
-    return {"status": "ok"}
-
+    author: str
 
 @router.post("/merge")
 async def merge_files() -> dict[str, str]:
@@ -82,6 +35,62 @@ async def merge_files() -> dict[str, str]:
     return {"status": "ok"}
 
 
+@router.get("/")
+async def get_files(auth: str = Header()) -> list[dict[str, FileModel]]:
+    user = await auth_check(auth)
+
+    result = []
+    username = user['username']
+
+    for key, value in files.items():
+        if value.author == username:
+            result.append({key: value})
+
+    return result
+
+
+@router.post("/")
+async def post_file(
+    auth: str = Header(),
+    input_file: UploadFile = File()
+) -> dict[str, FileModel]:
+    user = await auth_check(auth)
+
+    prefix = 'files/'
+    file_id = str(uuid.uuid4())
+    while file_id in files:
+        file_id = str(uuid.uuid4())
+    path = prefix + file_id + '.pdf'
+
+    with open(path, "wb") as buffer:
+        while chunk := await input_file.read(8192):
+            buffer.write(chunk)
+
+    new_file = FileModel(
+        filename=input_file.filename,
+        path=path,
+        author=user['username'],
+    )
+    files[file_id] = new_file
+
+    return {file_id: new_file}
+
+
+@router.get("/{id}")
+async def get_file_by_id() -> dict[str, str]:
+    return {"status": "ok"}
+
+
+@router.post("/{id}")
+async def post_file_by_id() -> dict[str, str]:
+    return {"status": "ok"}
+
+
+@router.delete("/{id}")
+async def delete_file_by_id() -> dict[str, str]:
+    return {"status": "ok"}
+
+
 async def introspect(auth: str):
     headers = {
         "accept": "application/json",
@@ -95,3 +104,11 @@ async def introspect(auth: str):
                 return None
             body = await response.text()
             return body
+
+
+async def auth_check(auth):
+    auth_response = await introspect(auth=auth)
+    if auth_response is None:
+        raise HTTPException(status_code=403, detail='Forbidden')
+
+    return json.loads(auth_response)
