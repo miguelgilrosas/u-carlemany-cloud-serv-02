af225b5e2507e327619cc169491c571584e9e6c5
miguelgilrosas <miguelgilrosas@gmail.com>
2025-03-16 15:00:57 +0000

    activity 2

--- app/main.py
+++ app/main.py
@@ -3,6 +3,27 @@ from fastapi import FastAPI
 from app.authentication.router import router as authentication_router
 from app.files.router import router as files_router
 
-app = FastAPI()
-app.include_router(authentication_router, prefix='/auth')
-app.include_router(files_router, prefix='/files')
+description = """
+# Universidad Carlemany
+
+## Báchelor de Informática
+
+### Cloud Computing Services
+
+Profesor: Ramón Amela
+"""
+
+metadata = [
+    {
+        "name": "Authentication",
+        "description": "Description of Authentication"
+    },
+    {
+        "name": "Files",
+        "description": "Description of Files"
+    }
+]
+
+app = FastAPI(title='Activity02', description=description, tags_metadata=metadata)
+app.include_router(authentication_router, prefix='/auth', tags=['Authentication'])
+app.include_router(files_router, prefix='/files', tags=['Files'])
